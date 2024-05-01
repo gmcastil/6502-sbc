3b77496462ff9ba39fe51ad2ad2e9c0aad6021e4
George Castillo <gmcastil@gmail.com>
2024-05-01 06:11:38 -0600

    Fixed an issue with MSB and LSB being reversed in the monitor

--- tools/6502-monitor.ino
+++ tools/6502-monitor.ino
@@ -1,3 +1,8 @@
+/*
+ * Address and data lines are listed LSB to MSB as they would be hooked up on
+ * the board (e.g., pin 24 is assumed to be connected to address pin 1 on the
+ * 65C02 IC).
+ */
 const unsigned int ADDR_PINS[] = {22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52};
 const unsigned int DATA_PINS[] = {39, 41, 43, 45, 47, 49, 51, 53};
 
@@ -8,7 +13,7 @@ const unsigned int DATA_PINS[] = {39, 41, 43, 45, 47, 49, 51, 53};
 void setup()
 {
 	/* Initialize the serial port */
-	Serial.begin(115200);
+	Serial.begin(57600);
 
 	/* Set each address pin as an input */
 	for (int i=0; i<16; i++) {
@@ -26,7 +31,6 @@ void setup()
 	/* Set up the clock as an interrupt source */
 	pinMode(CLOCK_PIN, INPUT);
 	attachInterrupt(digitalPinToInterrupt(CLOCK_PIN), clock_isr, RISING);
-  
 }
 
 /*
@@ -45,13 +49,13 @@ void clock_isr()
 	addr = 0;
 	data = 0;
 
-	/* Sample the address lines */
-	for (int i=0; i<16; i++) {
+	/* Sample the address lines MSB to LSB */
+	for (int i=15; i>=0; i--) {
 		read_bit = digitalRead(ADDR_PINS[i]) ? 1 : 0;
 		addr = (addr << 1) + read_bit;
 	}
-	/* Sample the data lines */
-	for (int i=0; i<8; i++) {
+	/* Sample the data lines MSB to LSB */
+	for (int i=7; i>=0; i--) {
 		read_bit = digitalRead(DATA_PINS[i]) ? 1 : 0;
 		data = (data << 1) + read_bit;
 	}
